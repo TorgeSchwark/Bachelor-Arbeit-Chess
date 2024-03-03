94ba08f7656378470344e6b90b21d971b2b818b0
TorgeSchwark <torge.schwark@outlook.de>
2024-03-03 18:29:00 +0000

    added find direction_moves and findJunpmoves

--- src/chess_implementation/find_moves.py
+++ src/chess_implementation/find_moves.py
@@ -28,6 +28,9 @@ def find_pawn_moves(chess_board: ChessBoard, piece: Piece, moves):
     
     pos_x = piece.position[0]
     pos_y = piece.position[1]
+    size = chess_board.size
+    color = chess_board.color_to_move
+    board = chess_board.board
 
     if piece.first_move == -1:
         move_directions = piece.rules.move_directions.copy()
@@ -49,45 +52,116 @@ def find_pawn_moves(chess_board: ChessBoard, piece: Piece, moves):
             real_range = rangee
 
         dist = 1
-        field_x  = pos_x
-        field_y  = pos_y
-        while dist <= rangee:
-            field_x += direction_x
-            field_y += direction_y
-            
-            #promotion need extra cas !!!!
-            if chess_board.board[field_x][field_y] == 0:
-                if dist <= real_range:
+        field_x  = pos_x+direction_x
+        field_y  = pos_y+direction_y
+        while dist <= rangee and on_board(field_x, size) and on_board(field_y,size):
+            #promotion need extra case !!!!
+            if board[field_x][field_y] == 0:
+                if dist <= real_range and (not promotion(field_y, size, color)):
                     add_move(pos_x, pos_y, field_x, field_y, moves, NORMAL_MOVE)
-                else:
+                elif dist >= real_range and (not promotion(field_y, size, color)):
                     add_move(pos_x, pos_y, field_x, field_y, moves, DOUBLE_PAWN)
+                elif promotion(field_y, color, size):
+                    add_promotions(pos_x, pos_y, field_x, field_y, moves, chess_board)
             else:
                 break
+
+            field_x += direction_x
+            field_y += direction_y
             dist += 1
     
     direction_y = chess_board.color_to_move
-    #diagonal capture Moves
-    if chess_board[field_x+1][field_y+direction_y] * chess_board[pos_x][pos_y] < 0:
-        add_move(pos_x,pos_y, field_x+1,field_y+direction_y, moves, NORMAL_MOVE)
-    if chess_board[field_x-1][field_y+direction_y] * chess_board[pos_x][pos_y] < 0:
-        add_move(pos_x,pos_y, field_x+1,field_y+direction_y, moves, NORMAL_MOVE)
 
+    #diagonal capture Moves(on_board(x,y) and gegner and not_promotion field)
+    if on_board(pos_x+1, size) and on_board(pos_y+direction_y, size) and (board[pos_x+1][pos_y+direction_y] * board[pos_x][pos_y] < 0) and (not promotion(pos_y +direction_y,size,color)):
+        add_move(pos_x, pos_y, pos_x+1,pos_y+direction_y, moves, NORMAL_MOVE)
+    elif on_board(pos_x+1, size) and on_board(pos_y+direction_y, size) and (board[pos_x+1][pos_y+direction_y] * board[pos_x][pos_y] < 0) and promotion(pos_y +direction_y,size,color):
+        add_promotions(pos_x, pos_y, pos_x+1,pos_y+direction_y, moves, chess_board)
+    if on_board(pos_x-1, size) and on_board(pos_y+direction_y, size) and (board[pos_x-1][pos_y+direction_y] * board[pos_x][pos_y] < 0) and (not promotion(pos_y +direction_y,size,color)):
+        add_move(pos_x,pos_y, pos_x+1,pos_y+direction_y, moves, NORMAL_MOVE)
+    elif on_board(pos_x-1, size) and on_board(pos_y+direction_y, size) and (board[pos_x-1][pos_y+direction_y] * board[pos_x][pos_y] < 0) and promotion(pos_y +direction_y,size,color):
+        add_promotions(pos_x,pos_y, pos_x+1,pos_y+direction_y, moves, chess_board)
+    
     #jump moves on a pawn dont make sense but any ways 
-    #add_jump_moves(chess_board: ChessBoard, piece: Piece, moves)
+    find_jump_moves(chess_board, piece, moves)
+
+def find_move_directions(chess_board: ChessBoard, piece: Piece, moves):
+
+    move_directions = piece.rules.move_directions
+
+    pos_x = piece.position[0]
+    pos_y = piece.position[1]
+    size = chess_board.size
+    color = chess_board.color_to_move
+    board = chess_board.board
+
+    for ind in range(len(move_directions)//3):
+        direction_x = move_directions[ind*3]
+        direction_y = move_directions[ind*3+1]
+        rangee = move_directions[ind*3+2]
+
+        dist = 1
+        field_x  = pos_x + direction_x
+        field_y  = pos_y + direction_y
+        while dist <= rangee and on_board(field_x, size) and on_board(field_y, size): 
+            
+            if board[field_x][field_y] == 0:
+                add_move(pos_x, pos_y, field_x, field_y, moves, NORMAL_MOVE)
+            elif board[field_x][field_y] * board[pos_x][pos_y] > 0:
+                break
+            elif board[field_x][field_y] * board[pos_x][pos_y] < 0:
+                add_move(pos_x, pos_y, field_x, field_y, moves, NORMAL_MOVE)
+
+            field_x += direction_x
+            field_y += direction_y
+            dist += 1
+
+
+def find_jump_moves(chess_board: ChessBoard, piece: Piece, moves):
+
+    jump_moves = piece.rules.jump_moves
+
+    pos_x = piece.position[0]
+    pos_y = piece.position[1]
+    size = chess_board.size
+    board = chess_board.board
+
+    for ind in range(len(jump_moves)//2):
+        field_x = pos_x + jump_moves[ind*2]
+        field_y = pos_y + jump_moves[ind*2+1]
+        
+        if on_board(field_y, size) and on_board(field_x, size) and board[field_x][field_y] <= 0 :
+            add_move(pos_x, pos_y, field_x, field_y, moves, NORMAL_MOVE)
+
+
+
+
 
 def add_move(from_x, from_y, to_x, to_y, moves: MoveStack, move_type):
-    if move_type != PROMOTION:
+    move_ind = moves.head
+    moves.stack[move_ind*5] = from_x
+    moves.stack[move_ind*5+1] = from_y
+    moves.stack[move_ind*5+2] = to_x
+    moves.stack[move_ind*5+3] = to_y
+    moves.stack[move_ind*5+4] = move_type
+    moves.head += 1
+
+    
+
+def add_promotions(from_x,from_y, to_x, to_y, moves: MoveStack, chess_board: ChessBoard):
+    for ind in range(len(chess_board.all_non_pawn_pieces)):
         move_ind = moves.head
         moves.stack[move_ind*5] = from_x
         moves.stack[move_ind*5+1] = from_y
         moves.stack[move_ind*5+2] = to_x
         moves.stack[move_ind*5+3] = to_y
-        moves.stack[move_ind*5+4] = move_type
+        moves.stack[move_ind*5+4] = ind
         moves.head += 1
-    else:
-        pass
 
-
+def promotion(y, color, size):
+    if (color == 1 and y == size-1) or (color == -1 and y == 0):
+        return True
+    return False
 
 def on_board(x,size):
     return (x >= 0 and x < size)
